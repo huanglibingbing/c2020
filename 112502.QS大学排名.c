a40c7dea182b2786886b46cee7d4d388143cec0e
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-11-25 09:46:29 +0800

    Create 112502.QS大学排名.c

---
+++ 112502.QS大学排名.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main(){
+	int a;
+	if(a==1){
+	printf("清华大学");
+	}
+	else if(a==2){
+	printf("北京大学");
+	}
+	else if(a==3){
+	printf("复旦大学");
+	}
+	else if(a==4){
+	printf("中国科技技术大学");
+	}
+	else if(a==5){
+	printf("浙江大学");
+	}
+	else if(a==6){
+	printf("上海交通大学");
+	}
+	else if(a==7){
+	printf("南京大学");
+	}
+	else if(a==8){
+	printf("中山大学");
+	}
+	else if(a==9){
+	printf("武汉大学");
+	}
+	else if(a==10){
+	printf("哈尔滨工业大学");
+	}
+	else{
+		printf("排名太低");}
+}
