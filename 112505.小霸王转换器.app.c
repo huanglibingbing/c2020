a4cf0b41a176edbb3f4eed32565df49f84d7836b
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-11-25 20:21:27 +0800

    Create 112505.小霸王转换器.app.c

---
+++ 112505.小霸王转换器.app.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdio.h>
+void main(){
+	while(1){
+	int a=5;
+	int c;
+	printf("友情提示：只能输入0-10\n");
+	scanf("%d",&c);
+	if(c==5){
+	printf("答案正确，点赞!\n");}
+	else if(c<5){
+	printf("答案太小了!\n");
+	}
+	else{
+	printf("答案太大了!\n");
+	}
+}
+}
