dddb6489ee44346ebb1afa0cefc882136f8725dd
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-12-02 16:46:11 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -5,10 +5,11 @@ void main(){
 	scanf("%d",&a);
 	if(a>86400)
 	printf("输入错误\n");
-	else
+	else{
 		b=a/3600;
 		c=(a%3600)/60;
 		d=a-3600*b-60*c;
+	}
 	printf("%02d时",b);
 	printf("%02d分",c);
 	printf("%02d秒",d);
