fdfbbc08154698905a1d159ad7726be5de0d85be
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-12-02 16:44:53 +0800

    Create 120203.秒转时分秒.app.c

---
+++ 120203.秒转时分秒.app.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+	int a,b,c,d;
+	printf("北京时间：\n");
+	scanf("%d",&a);
+	if(a>86400)
+	printf("输入错误\n");
+	else
+		b=a/3600;
+		c=(a%3600)/60;
+		d=a-3600*b-60*c;
+	printf("%02d时",b);
+	printf("%02d分",c);
+	printf("%02d秒",d);
+
+}
