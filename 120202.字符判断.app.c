a40177d39bea5d843f4c54b3640f8152362b4a95
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-12-02 09:30:32 +0800

    Update 120202.字符判断.app.c

--- 120202.字符判断.app.c
+++ 120202.字符判断.app.c
@@ -4,10 +4,10 @@ void main(){
 	printf("欢迎使用:\n");
 	scanf("%c",&e);
 	switch(e){
-	case 'a':printf("你在点击"上"健");break;
-	case 's':printf("你在点击"下"健");break;
-	case 'd':printf("你在点击"左"健");break;
-	case 'w':printf("你在点击"右"健");break;
+	case 'a':printf("你在点击上健");break;
+	case 's':printf("你在点击下健");break;
+	case 'd':printf("你在点击左健");break;
+	case 'w':printf("你在点击右健");break;
 
 	default:printf("输入错误\n");
 	}
