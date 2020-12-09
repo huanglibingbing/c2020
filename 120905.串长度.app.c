4cfa5b30bafee3822e7e812a439ca8deff992dd8
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-12-09 17:04:59 +0800

    Create 120905.串长度.app.c

---
+++ 120905.串长度.app.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+        char a[]="change";
+        char b[]="真不错";
+        printf("%d",sizeof(a));
+         printf("\n");
+         printf("%d",sizeof(b));
+
+}
+
