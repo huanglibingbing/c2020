322dca81211dc870ca9230c60a6fd67499d2e48b
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-12-09 16:48:58 +0800

    Create 120904.串对比.app.c

---
+++ 120904.串对比.app.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+        char a[]="小韦";
+        char b[]="小韦";
+        if(strcmp(a,b)==0){
+            printf("相同");
+        }
+        else{
+            printf("不相同");
+        }
+}
+
