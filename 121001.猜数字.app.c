dab93cae0b35013e50b9362ca6808c31ab04b97f
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-12-10 21:14:25 +0800

    Create 121001.猜数字.app.c

---
+++ 121001.猜数字.app.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char c[]="嘿";
+    char d[20];
+    int a=13;
+    int b;
+        printf("欢迎\n");
+    while(1){
+        scanf("%s",&d);
+    if(strcmp(c,d)==0){
+        printf("开始\n");
+        scanf("%d",&b);
+    if(b==13){
+        printf("正确\n");
+    }
+    
+    else if(b<13){
+        printf("太小了\n");
+    }
+    else {
+        printf("太大了\n");
+    }
+	}
+	else {
+		printf("错误\n");
+	
+	}
+}
+
+}
