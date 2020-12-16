1e824ba046580948dc8571f5461c15ebaafd444a
huanglibingbing <74294630+huanglibingbing@users.noreply.github.com>
2020-12-16 09:29:20 +0800

    Create 121602.常规求和.app.c

---
+++ 121602.常规求和.app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int a;
+            scanf("%d",&a);
+    if(a>=0){
+            printf("%d",(1+a)*a/2);
+            }
+    else {
+            printf("%d\n",a);
+}
+}
+
